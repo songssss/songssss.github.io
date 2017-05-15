49fe59c045998975bd361ac13923f8b3f44426c1
songssss <snow6348@naver.com>
2017-05-15 11:51:42 +0900

    add service worker

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  /*
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+	*/
+
+	if ('serviceWorker' in navigator) {
+	  window.addEventListener('load', function() {
+		navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+		  // Registration was successful
+		  console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		}).catch(function(err) {
+		  // registration failed :(
+		  console.log('ServiceWorker registration failed: ', err);
+		});
+	  });
+	}
+
+
+})();
